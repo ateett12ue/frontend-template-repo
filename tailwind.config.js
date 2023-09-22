0ccd0cd6fe20a2e1819b48ea821ed966893f0fa0
ateett12ue <ateettiwari97@gmail.com>
2023-09-22 12:48:05 +0530

    tailwind intialise

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+// tailwind.config.js
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      // Set font family
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      // Set theme colors (Required config!)
+      colors: {
+        primary: colors.blue,
+        secondary: colors.slate,
+      },
+    },
+  },
+  // Add plugins
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+};
